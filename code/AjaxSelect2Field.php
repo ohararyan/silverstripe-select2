60c41c1aa29fe7d111c301cc0f85e1fd80fceb85
Ryan O'Hara <ryan.a.ohara@gmail.com>
2015-11-24 21:18:28 +1300

    starting to update to new select2 version

--- code/AjaxSelect2Field.php
+++ code/AjaxSelect2Field.php
@@ -5,7 +5,7 @@
  * http://ivaynberg.github.io/select2/
  * @author shea@livesource.co.nz
  **/
-class AjaxSelect2Field extends TextField{
+class AjaxSelect2Field extends DropdownField{
 
 	private static $allowed_actions = array('search');
 
@@ -22,17 +22,111 @@ class AjaxSelect2Field extends TextField{
 		'multiple'				=> false,
 	);
 
+	protected $sourceObject, $keyField;
 
-	function Field($properties = array()){
-		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
-		Requirements::javascript(THIRDPARTY_DIR . '/jquery-entwine/dist/jquery.entwine-dist.js');
-		Requirements::javascript(SELECT2_MODULE . "/select2/select2.js");
-		Requirements::javascript(SELECT2_MODULE . '/javascript/ajaxselect2.init.js');
-		Requirements::css(SELECT2_MODULE . "/select2/select2.min.css");
-			Requirements::css(SELECT2_MODULE . "/css/ajaxSelect2.css");
-		return parent::Field($properties);
+	/**
+	 * @param string $name
+	 * @param string $title
+	 * @param null|DataList $source
+	 * @param null|DataList $value
+	 */
+	public function __construct($name, $title = '', $source = array(), $value = '') {
+
+		$this->sourceObject = $source;
+		$this->keyField     = $value;
+
+		parent::__construct($name, $title, $source, $value);
 	}
 
+	public function Field($properties = array()){
+		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
+		Requirements::javascript(THIRDPARTY_DIR . '/jquery-entwine/dist/jquery.entwine-dist.js');
+
+		Requirements::javascript(SELECT2_MODULE . "/select2/select2.js");
+		Requirements::javascript(SELECT2_MODULE . '/javascript/ajaxselect2.init.js');
+
+		Requirements::css(SELECT2_MODULE . "/select2/select2.min.css");
+		Requirements::css(SELECT2_MODULE . "/css/ajaxSelect2.css");
+
+		var_dump($this->value);
+		var_dump($this->Value());
+
+
+		// $record = $this->Value() ? $this->objectForKey($this->Value()) : null;
+		// var_dump($record);
+
+		$properties = array_merge($properties, array(
+			'Options' => $this->getOptions()
+		));
+
+		// return parent::Field($properties);
+
+		return $this
+				->customise($properties)
+				->renderWith(array("templates/AjaxSelect2Field"));
+	}
+
+	protected function getOptions() {
+		$options = array();
+		$source = $this->getSource();
+
+		if(is_object($source)) {
+			$options[] = new ArrayData(array(
+				'Value' => '1',
+				'Title' => 'Hello'
+			));
+		}
+
+		// if($source) {
+
+			// var_dump($source);
+
+		// 	foreach($source as $value => $title) {
+		// 		$selected = false;
+
+		// 		var_dump($value);
+
+		// 		$options[] = new ArrayData(array(
+		// 			));
+		// 	}
+		// }
+
+		// return $options;
+
+		// $dataClass = $source->dataClass();
+
+		$values = $this->Value();
+
+
+		var_dump($options);
+		die();
+		return $options;
+
+		// // var_dump($source);
+		// // var_dump($values);
+
+		// if(!$values) {
+		// 	return $options;
+		// }
+
+
+
+	}
+
+
+	/**
+	 * Get the object where the $keyField is equal to a certain value
+	 *
+	 * @param string|int $key
+	 * @return DataObject
+	 */
+	protected function objectForKey($key) {
+		if($this->keyField == 'ID') {
+			return DataObject::get_by_id($this->sourceObject, $key);
+		} else {
+			return DataObject::get_one($this->sourceObject, "\"{$this->keyField}\" = '".Convert::raw2sql($key)."'");
+		}
+	}
 
 	public function search($request){
 		$list = DataList::create($this->getConfig('classToSearch'));
@@ -89,7 +183,6 @@ class AjaxSelect2Field extends TextField{
 		$attributes = array_merge(
 			parent::getAttributes(),
 			array(
-				'type' => 'hidden',
 				'data-searchurl' => $this->Link('search'),
 				'data-minimuminputlength' => $this->getConfig('minimumInputLength'),
 				'data-resultslimit' => $this->getConfig('resultsLimit'),
@@ -98,6 +191,9 @@ class AjaxSelect2Field extends TextField{
 			)
 		);
 
+
+
+		// var_dump($this->ID());
 		if($this->Value() && $object = DataObject::get($this->getConfig('classToSearch'))->byID($this->Value())){
 			$originalSourceFileComments = Config::inst()->get('SSViewer', 'source_file_comments');
 			Config::inst()->update('SSViewer', 'source_file_comments', false);
--- javascript/ajaxselect2.init.js
+++ javascript/ajaxselect2.init.js
@@ -1,67 +1,143 @@
 (function($) {
-	$.entwine("select2", function($) {
-		$("input.ajaxselect2").entwine({
-			onmatch: function() {
-				var self = this;
-				self.select2({
+	$.entwine('select2', function($){
+		$('select.ajaxselect2').entwine({
+			applySelect2: function() {
+				var self = this,
+					$select = $(this);
+
+				// There is a race condition where Select2 might not
+				// be bound to jQuery yet. So here we make sure Select2
+				// // is defined before trying to invoke it.
+				// if ($.fn.select2 === void 0) {
+				// 	console.log('something');
+				// 	return setTimeout(function () {
+				// 		console.log('something else');
+
+				// 		self.applySelect2();
+				// 	}, 0);
+				// }
+
+				$select.select2({
 				    placeholder: self.data('placeholder'),
 				    minimumInputLength: self.data('minimuminputlength'),
 				    page_limit: self.data('resultslimit'),
 				    quietMillis: 100,
 				    ajax: {
-				        url: self.data('searchurl'),
+				        url: $select.data('searchurl'),
 				        dataType: 'json',
-				        data: function (term, page) {
-				            return {
-				                term: term,
-				                page: page
-				            };
+				        type: 'GET',
+				        delay: 250,
+				        data: function (params) {
+				        	// console.log(params);
+				        	return {
+				        		term: params.term,
+				        		page: params.page
+				        	};
 				        },
-				        results: function (data, page) {
-				        	var more = (page * self.data('resultslimit')) < data.total
+				        processResults: function (data, params) {
+
+				        	console.log(data, params);
+
+				        	params.page = params.page || 1;
+
 				            return {
-				            	results: data.list,
-				            	more: more
+				            	results: data.list
 				           	};
-				        }
+				        },
+    				    cache: true
 				    },
-				    initSelection: function(element, callback) {
-				    	console.log('INISEL')
-				        callback($(element).data('selectioncontent'));
-				    },
-				    formatResult: function(item) {
+				    templateResult: function(item) {
+				    	console.log(item);
 				        return item.resultsContent;
 				    },
-    				formatSelection: function(item) {
-    					if(item.selectionContent){
-    						return item.selectionContent;
-    					}else{
-    						return item;
-    					}
+    				templateSelection: function(data) {
+    					console.log(data);
+    					return data.label || data.text;
+    					// console.log(item);
+    					// if(item.selectionContent){
+    					// 	return item.selectionContent;
+    					// }else{
+    					// 	return item;
+    					// }
 
 				    },
-				    dropdownCssClass: "bigdrop",
-				    escapeMarkup: function (m) { return m; }
+				    allowClear: true,
+				    escapeMarkup: function (markup) { return markup; }
 				});
 			},
-			onchange: function() {
-				if($(this).val()) {
-					$(this).next('a.ajaxselect2Remove').show();
-				} else {
-					$(this).next('a.ajaxselect2Remove').hide();
-				}
+			onmatch: function() {
+				this.applySelect2();
+				// console.log(this);
 			}
 		});
-
-		// ability to remove set option -
-		// only for Ajax version
-		$("a.ajaxselect2Remove").entwine({
-			onclick: function() {
-				var input = $(this).siblings('input');
-				input.select2('data', {id: null, text: null});
-				input.trigger('change')
-			}
-		});
-
 	});
-}(jQuery));
+})(jQuery);
+
+
+// (function($) {
+// 	$.entwine("select2", function($) {
+// 		$("input.ajaxselect2").entwine({
+// 			onmatch: function() {
+// 				var self = this;
+// 				self.select2({
+// 				    placeholder: self.data('placeholder'),
+// 				    minimumInputLength: self.data('minimuminputlength'),
+// 				    page_limit: self.data('resultslimit'),
+// 				    quietMillis: 100,
+// 				    ajax: {
+// 				        url: self.data('searchurl'),
+// 				        dataType: 'json',
+// 				        data: function (term, page) {
+// 				            return {
+// 				                term: term,
+// 				                page: page
+// 				            };
+// 				        },
+// 				        results: function (data, page) {
+// 				        	var more = (page * self.data('resultslimit')) < data.total
+// 				            return {
+// 				            	results: data.list,
+// 				            	more: more
+// 				           	};
+// 				        }
+// 				    },
+// 				    initSelection: function(element, callback) {
+// 				    	console.log('INISEL')
+// 				        callback($(element).data('selectioncontent'));
+// 				    },
+// 				    formatResult: function(item) {
+// 				        return item.resultsContent;
+// 				    },
+//     				formatSelection: function(item) {
+//     					if(item.selectionContent){
+//     						return item.selectionContent;
+//     					}else{
+//     						return item;
+//     					}
+
+// 				    },
+// 				    dropdownCssClass: "bigdrop",
+// 				    escapeMarkup: function (m) { return m; }
+// 				});
+// 			},
+// 			onchange: function() {
+// 				if($(this).val()) {
+// 					$(this).next('a.ajaxselect2Remove').show();
+// 				} else {
+// 					$(this).next('a.ajaxselect2Remove').hide();
+// 				}
+// 			}
+// 		});
+
+// 		// ability to remove set option -
+// 		// only for Ajax version
+// 		$("a.ajaxselect2Remove").entwine({
+// 			onclick: function() {
+// 				var input = $(this).siblings('input');
+// 				input.select2('data', {id: null, text: null});
+// 				input.trigger('change')
+// 			}
+// 		});
+
+// 	});
+// }(jQuery));
